eb82a22b358167287f4db3c376ca94dd4fad32e0
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2020-10-14 11:58:18 -0300

    Update separacion_conjuntos.R

--- scripts/separacion_conjuntos.R
+++ scripts/separacion_conjuntos.R
@@ -1,8 +1,13 @@
 # Nombre de archivo: separacion_conjuntos.R
 # Función: Separa en entrenamiento, testeo y enero según inputs
 
-separacion_conjuntos = function(dataset, periodos_train, periodos_test){
-  enero <<- ds[foto_mes == 202001,] # La doble flecha es para asignar al entorno global
-  ds_train <<- ds[foto_mes %in% periodos_train]
-  ds_test <<- ds[foto_mes %in% periodos_test] 
+separacion_conjuntos = function(dataset, periodos_train, periodos_test, proporcion_columnas){
+  enero = ds[foto_mes == 202001,] # La doble flecha es para asignar al entorno global
+  ds_train = ds[foto_mes %in% periodos_train]
+  ds_test = ds[foto_mes %in% periodos_test]
+  nu_columnas = round(ncol(ds_train) * proporcion_columnas, 0)
+  columnas = unique(c("foto_mes", "numero_de_cliente", sample(names(ds_train), nu_columnas), "clase_binaria"))
+  ds_train <<- ds_train[, columnas, with=FALSE]
+  ds_test <<- ds_test[, columnas, with=FALSE]
+  enero <<- enero[, columnas, with=FALSE]
 }
