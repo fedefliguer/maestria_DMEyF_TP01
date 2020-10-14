5ee7e165602de5d9c0a2633811a12e5b9d657698
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2020-10-14 18:01:49 -0300

    Create rpart_gridsearch.R

---
+++ scripts/modelos/rpart_gridsearch.R
@@ -0,0 +1,53 @@
+rpart_gridsearch = function(dataset_train, dataset_test){
+  mejor_params <- list(  "ganancia"= -1,
+                         "maxdepth"= -1,
+                         "minsplit"= -1,
+                         "minbucket"= -1)
+  
+  for( vmaxdepth  in max_depths)    
+  {
+    for(vminsplit in  min_splits)
+    {
+      for(vminbucket in  min_buckets)
+      {
+        modelo   <-  rpart("clase_binaria ~ .",  
+                           data = dataset_train[, !c("foto_mes", "numero_de_cliente"), with=FALSE],
+                           model= TRUE,
+                           xval=0,
+                           cp= 0,
+                           maxdepth=  vmaxdepth,
+                           minsplit=  vminsplit,
+                           minbucket=  vminbucket
+        )
+        
+        prediction_test  <- predict( modelo, dataset_test[, !c("foto_mes", "numero_de_cliente"), with=FALSE], type = "prob")
+        
+        entrega <-   as.data.table(cbind( "numero_de_cliente" = dataset_test[, numero_de_cliente],
+                                          "prob" =prediction_test[, "evento"],
+                                          "clase_binaria" = dataset_test[, clase_binaria])
+        )
+        
+        entrega[  ,  estimulo :=  as.integer( prob > 0.025)]
+        
+        ganancia <- sum(  entrega[ estimulo==1, ifelse( clase_binaria=="evento", 29250, -750) ])
+        
+        if( ganancia > mejor_params$ganancia )
+        {
+          mejor_params$ganancia  <- ganancia
+          mejor_params$maxdepth  <- vmaxdepth
+          mejor_params$minsplit  <- vminsplit
+          mejor_params$minbucket <- vminbucket
+        }
+      }
+    }
+  }
+  
+  modelo  <<-  rpart ("clase_binaria ~ .",  
+                      data = dataset_train[, !c("foto_mes", "numero_de_cliente"), with=FALSE], 
+                      model= TRUE,
+                      xval=0,
+                      cp= 0,
+                      maxdepth=   mejor_params$maxdepth,
+                      minsplit=   mejor_params$minsplit,
+                      minbucket=  mejor_params$minbucket)
+}
