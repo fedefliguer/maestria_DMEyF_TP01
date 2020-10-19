6051c49a3e4009578685a848075e18a938188eaf
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2020-10-19 14:39:30 -0300

    Update variables_historicas_mmovil.R

--- scripts/variables_historicas_mmovil.R
+++ scripts/variables_historicas_mmovil.R
@@ -86,7 +86,9 @@ variables_historicas_mmovil = function(dataset, periodos){
   dataset[, paste0(rollear, "__ravg") := frollmean(.SD, periodos),
      .SDcols = rollear,
      by = numero_de_cliente]
-  nuevo_orden <-  c( setdiff( colnames(dataset ) , "clase_binaria" ) , "clase_binaria" )
+  dataset <- dataset[,colSums(is.na(dataset))<nrow(dataset)]
+  
   setcolorder( dataset, nuevo_orden )
+  nuevo_orden <-  c( setdiff( colnames(dataset ) , "clase_binaria" ) , "clase_binaria" )
   remove(campo, campo_idx, col_original, columnas_no_procesar, columnas_originales, columnas_originales_a_procesar, i, last, nueva_col, vector_desde, vector_ids, ventana_regresion)
   }
