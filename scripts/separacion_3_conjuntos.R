09dce6f82426d825b2263031dbf456095be19a53
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2020-10-20 10:51:46 -0300

    Update separacion_3_conjuntos.R

--- scripts/separacion_3_conjuntos.R
+++ scripts/separacion_3_conjuntos.R
@@ -1,7 +1,7 @@
 # Nombre de archivo: separacion_conjuntos.R
 # Función: Separa en entrenamiento, testeo y enero según inputs
 
-separacion_conjuntos = function(dataset, periodos_train, periodos_test_1, periodos_test_2, proporcion_columnas=1, proporcion_filas=1){
+separacion_3_conjuntos = function(dataset, periodos_train, periodos_test_1, periodos_test_2, proporcion_columnas=1, proporcion_filas=1){
   enero = ds[foto_mes == 202001,] # La doble flecha es para asignar al entorno global
   ds_train = ds[foto_mes %in% periodos_train]
   ds_test_1 = ds[foto_mes %in% periodos_test_1]
